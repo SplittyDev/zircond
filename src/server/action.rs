fafde94874c856791c311519430d82d63cba8352
splittydev <splittydev@protonmail.com>
2018-08-23 19:37:44 +0200

    Improve documentation

--- src/server/action.rs
+++ src/server/action.rs
@@ -1,15 +1,54 @@
 #[derive(Debug)]
 pub enum IrcAction {
 
+    //
     // Users
+    //
+
+    /// User / Connect
     UserConnect(),
+
+    /// User / Set Nick
+    /// 
+    /// * `0` - Nickname
     UserSetNick(String),
+
+    /// User / Set Names
+    /// 
+    /// * `0` - Username
+    /// * `1` - Realname
     UserSetNames(String, Option<String>),
+
+    /// User / Join Channel
+    /// 
+    /// * `0` - Channel name
     UserJoinChannel(String),
 
+    //
     // Channels
+    //
+    
+    /// Channel / List Users
+    /// 
+    /// * `0` - Channel name
     ChannelListUsers(String),
 
+    //
+    // Messaging
+    //
+
+    /// Private message
+    /// 
+    /// * `0` - Sender
+    /// * `1` - Recipient
+    Privmsg(String, String),
+
+    //
     // Misc
+    //
+
+    /// Reply to ping
+    /// 
+    /// * `0` - Ping id
     Pong(String),
 }
