b9be1366e1e08280f79aac2d97f2be896960fe6b
splittydev <splittydev@protonmail.com>
2018-08-24 23:26:20 +0200

    Add functions for finding a user by nickname

--- src/server/user_list.rs
+++ src/server/user_list.rs
@@ -23,6 +23,14 @@ impl UserList {
         self.users.iter_mut().find(|user| user.id == client_id)
     }
 
+    pub fn find_by_name(&self, nick: &str) -> Option<&User> {
+        self.users.iter().find(|user| user.nickname() == nick)
+    }
+
+    pub fn find_by_name_mut(&mut self, nick: &str) -> Option<&mut User> {
+        self.users.iter_mut().find(|user| user.nickname() == nick)
+    }
+
     pub fn disconnect(&mut self, client_id: usize) -> bool {
         self.users.iter().position(|user| user.id == client_id).map(|user| self.users.remove(user)).is_some()
     }
