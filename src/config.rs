c1957addd165eab04defa229ffa37be7a8130554
splittydev <splittydev@protonmail.com>
2018-08-27 20:58:27 +0200

    Create default config if none exists

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,9 @@
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct ServerConfig {
     server: ServerConfigServer,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct ServerConfigServer {
     listen: String,
     host: String,
@@ -11,12 +11,30 @@ pub struct ServerConfigServer {
 }
 
 impl ServerConfig {
-    
+
     pub fn get_host(&self) -> &str {
         self.server.host.as_ref()
     }
 
     pub fn get_addr(&self) -> (&str, u16) {
         (self.server.listen.as_ref(), self.server.port)
+    }
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            server: ServerConfigServer::default(),
+        }
+    }
+}
+
+impl Default for ServerConfigServer {
+    fn default() -> Self {
+        Self {
+            listen: "127.0.0.1".to_string(),
+            host: "127.0.0.1".to_string(),
+            port: 6667,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,46 @@ use server::Server;
 use config::ServerConfig;
 
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 
 fn main() -> std::io::Result<()> {
 
-    // Read config
-    let config = read_config()?;
+    // Read or create configuration
+    let config = {
+        if std::path::Path::new("config.toml").exists() {
+            read_config()
+        } else {
+            create_config()
+        }
+    }?;
 
-    // Create server with config
+    // Create server
     let mut server = Server::new(config);
 
     // Listen
     server.listen();
+
+    // Exit with no errors
     Ok(())
+}
+
+fn create_config() -> std::io::Result<ServerConfig> {
+
+    // Create default configuration
+    let config = ServerConfig::default();
+
+    // Serialize config to string
+    if let Ok(config_text) = toml::to_string(&config) {
+        
+        // Write to file
+        let mut file = File::create("config.toml")?;
+        file.write_all(config_text.as_ref())?;
+
+        // Return the configuration
+        Ok(config)
+    } else {
+        Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to serialize default configuration."))
+    }
 }
 
 fn read_config() -> std::io::Result<ServerConfig> {
@@ -41,6 +68,6 @@ fn read_config() -> std::io::Result<ServerConfig> {
     if let Ok(config) = toml::from_str(&config_text) {
         Ok(config)
     } else {
-        Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to parse config."))
+        Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to deserialize configuration."))
     }
 }
