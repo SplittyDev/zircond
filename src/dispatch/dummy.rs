c3b9ec4f8dda728a69e7fd8b6515dc3330845d3c
splittydev <splittydev@protonmail.com>
2018-08-28 21:40:46 +0200

    Update dummy dispatcher

--- src/dispatch/dummy.rs
+++ src/dispatch/dummy.rs
@@ -3,9 +3,11 @@ use std::net::TcpStream;
 use crate::server::Server;
 use crate::dispatch::command_dispatch::CommandDispatch;
 
-pub struct Dummy;
-impl CommandDispatch for Dummy {
-    fn dispatch(server: &mut Server, client: &mut TcpStream, client_id: usize, params: &[String]) {
+pub struct Dummy {
+    // Parameters
+}
 
+impl CommandDispatch for Dummy {
+    fn dispatch(&self, server: &mut Server, client: &mut TcpStream, client_id: usize) {
     }
 }
